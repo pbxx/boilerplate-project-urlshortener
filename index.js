369e42524cd4d54a0cbceeec79343e9de358d8ba
Donavan Lewis <airlew@gmail.com>
2024-07-28 00:10:40 -0600

    link shortening working up to fCC spec

--- index.js
+++ index.js
@@ -1,24 +1,74 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const app = express();
+require("dotenv").config()
+const fs = require("node:fs")
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const app = express()
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
+let state = {
+	links: [],
+}
 
-app.use(cors());
+const data = {
+	save() {
+		fs.writeFileSync(process.cwd() + "/data/state.json", JSON.stringify(state))
+	},
+	load() {
+		if (!fs.existsSync(process.cwd() + "/data")) {
+			// create the data folder if it doesn't exist
+			fs.mkdirSync(process.cwd() + "/data")
+			// create json file with current state data
+			data.save()
+		} else {
+			// data folder exists, load data json file if it exists
+			if (fs.existsSync(process.cwd() + "/data/state.json")) {
+				state = JSON.parse(fs.readFileSync(process.cwd() + "/data/state.json").toString())
+			} else {
+				// state json file doesnt exist, create it
+				data.save()
+			}
+		}
+	},
+}
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(cors())
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.use("/public", express.static(`${process.cwd()}/public`))
+
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html")
+})
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get("/api/shorturl/:urlId", function (req, res) {
+	if (req.params.urlId && state.links[parseInt(req.params.urlId)]) {
+		// short link found
+    res.redirect(state.links[parseInt(req.params.urlId)].original_url)
+	} else {
+		res.json({ error: "No short URL found for the given input" })
+	}
+	
+})
+app.post("/api/shorturl/", bodyParser.urlencoded({extended: false}), function (req, res) {
+	if (req.body.url) {
+		// short link found
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+    state.links.push({ original_url: req.body.url })
+    const urlId = state.links.length - 1
+    data.save()
+    res.json({original_url: req.body.url, short_url: urlId})
+	} else {
+		res.json({ error: "No short URL found for the given input" })
+	}
+	
+})
+
+// load/init state data
+data.load()
+
+// start app
+app.listen(port, function () {
+	console.log(`Listening on port ${port}`)
+})
